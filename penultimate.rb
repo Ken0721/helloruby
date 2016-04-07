2c418a192b77ae53048c1c85fedcb4395c169fab
ken.bean <kishiwda_kn@yahoo.co.jp>
2016-04-07 18:25:27 +0900

    Update penultimate.rb
    update

--- penultimate.rb
+++ penultimate.rb
@@ -1,16 +1,45 @@
-=begin
- Sample code to read in test cases:
-File.open(ARGV[0]).each_line do |line|
 # Do something with line, ignore empty lines
 # #...
 # end
 =end
 
+#check if there is a argument.
 if ARGV[0].nil? then
         puts "Missing arguments. Usage: ruby penultimate.rb <filename>"
         exit(1)
 end
 
+#check if there is the file of the argument.
+require "find"
+flag = 0
+Find.find("./"){|f|
+  tmp = File.basename(f)
+   #check match to hsdpSettingCDS***rpm
+  if ARGV[0] == tmp then
+    flag = 1
+    break
+  elsif tmp=~/^cdsPak.*rpm\z/ then
+    flag = 0
+    next
+  end
+}
+
+if flag == 0
+  puts "There is no such file,  " + ARGV[0] + "."
+  exit (1)
+end
+
+words=0
+w_num=0
 File.open(ARGV[0]).each_line do |line|
-        puts line
+  line.chomp!
+  words = line.split(/\s+/).reject{|w| w.empty?}
+  w_num += words.size
+  if w_num == 1
+    puts "#{words}"
+    puts "#{w_num}"
+  elsif
+    puts "#{words}"
+    puts "#{w_num}"
+  end
 end
