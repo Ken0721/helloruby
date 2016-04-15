9cefa3dc719c013c9df7da05ea21b82f7e52a59d
絹之介 <kennosuke@Kennosukes-MBP.local>
2016-04-15 20:54:17 +0900

    add ensyu1.rb

---
+++ ensyu1.rb
@@ -0,0 +1,45 @@
+class Library
+    attr_accessor :books
+
+    def initialize()
+        @books = []
+    end
+
+    def addBook(book)
+#        p book.title
+        @books.push(book)
+    end
+
+    def to_s
+#        puts "test"
+#        p @books
+        for b in @books do
+#            p "test"
+            puts "Title:" + b.title + ", "  + "Author:" + b.author
+        end
+    end
+
+end
+
+class Book
+    attr_accessor :author, :title
+    def initialize(args)
+        @author = args[:author]
+        @title = args[:title]
+        @library = args[:library]
+        @library.addBook(self)
+     #   p self.title
+    end
+end
+
+my_library = Library.new
+Book.new(:author => "Herman Melville", :title => "Moby-Dick", :library => my_library)
+Book.new(:author => "Hans Christian Andersen", :title => "The Ugly Duckling", :library => my_library)
+puts my_library
+#
+#
+#
+#
+#Library contents:
+#    Title: Moby-Dick, Author: Herman Melville
+#Title: The Ugly Duckling, Author: Hans Christian Andersen
